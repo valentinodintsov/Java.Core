6fdf5e3975f5106f0d1cbf9a7b6e1f00425271c7
valentinodintsov <odintsov.vn@gmail.com>
2019-06-08 23:57:23 +0500

    Удалил дубли

--- Lection2/ArrayException/MyArraySizeException.java
+++
@@ -1,9 +0,0 @@
-package ArrayException;
-
-public class MyArraySizeException extends Exception{
-
-    public MyArraySizeException() {
-        super("Массив не обладает размерностью 4х4!");
-    }
-}
-
